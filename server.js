f991a6d05dbfe38180e19f83fe12d597a520f6ab
KH <75588413+khallslearn@users.noreply.github.com>
2022-09-27 20:37:50 -0400

    modified:   Procfile 	modified:   server.js

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 const mongoose = require("mongoose");
 const connectDB = require("./config/database");
-//const TodoTask = require("./models/TodoTask");
 const homeRoutes = require("./routes/home")
 require('dotenv').config({path: './config/.env'})
 
