17abca5d5d1aa72261577e0642eb6de581c4c7ef
KH <75588413+khallslearn@users.noreply.github.com>
2022-10-01 16:58:56 -0400

    modified:   controllers/home.js 	modified:   models/nbatask.js 	modified:   views/index.ejs 	modified:   views/pages/search.ejs

--- controllers/home.js
+++ controllers/home.js
@@ -11,83 +11,20 @@ module.exports = {
             if (err) return res.status(500).send(err);
         }
     },
-    /*createTask: async (req, res) => {
-    
-        const todoTask = new nbaTask(
-            {
-                player_name: req.query.player_name,
-                team_abbreviation: req.query.team_abbreviation,
-                pts: req.query.pts,
-                reb: req.body.reb,
-                ast: req.body.ast
-
-            });
-        try {
-            await todoTask;
-            console.log(todoTask)
-            res.redirect("/");
-        } catch (err) {
-            if (err) return res.status(500).send(err);
-            res.redirect("/");
-        }
-    }*/
     searchTask: async(req,res)=>{
         try {
             const tasks = await
-            nbaTask.find({player_name:req.query.player_name});
+            nbaTask.findOne({player_name:req.query.player_name});
+            const task2 = await
+            nbaTask.findOne({player_name:req.query.player_name2});
             console.log(req.query.player_name)
-            res.render('pages/search', { data: tasks });
-        } catch (err) {
+            console.log(req.query.player_name2)
+            res.render('pages/search', {
+                 nbaTask: tasks,
+                nbaTask2: task2 });
+        }catch (err) {
             if (err) return res.status(500).send(err);
         }
     },
-    createTask: async (req, res) => {
-    
-        try {
-            var books = new nbaTask({
-                player_name:req.body.player_name,
-                pts: req.body.pts
-            });
-            books.save((err,data)=>{
-                if(err){
-                    console.log(err)
-                }else{
-                    res.redirect('/');
-                }
-            })
-     } catch (error) {
-         console.log(error);
-     }
-        }
     }
-    /*get('/search',(req,res)=>{
-    a    try {
-                 bookModel.find({$or:[{author:{'$regex':req.query.dsearch}},{books:{'$regex':req.query.dsearch}}]},(err,data)=>{
-                     if(err){
-                         console.log(err);
-                     }else{
-                         res.render('pages/home',{data:data});
-                     }
-                 })
-        } catch (error) {
-            console.log(error);
-        }
-    });
-    
-.post('/',(req,res)=>{
-        try {
-               var books = new bookModel({
-                   author:req.body.author,
-                   books:req.body.book
-               });
-               books.save((err,data)=>{
-                   if(err){
-                       console.log(err)
-                   }else{
-                       res.redirect('/');
-                   }
-               })
-        } catch (error) {
-            console.log(error);
-        }
-    });*/
+   
